4fa2f5a9622ef871ba1a9c54c9ef04adb3279ea1
Julian Easterling <julian@julianscorner.com>
2022-07-01 20:23:43 -0400

    Updated the build script to use the standard build steps.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,14 @@
-docker build -t dcjulian29/elasticsearch-curator:5.8.3 .
-docker tag dcjulian29/elasticsearch-curator:5.8.3 dcjulian29/elasticsearch-curator:latest
-docker push dcjulian29/elasticsearch-curator --all-tags
+@echo off
+setlocal
+
+pushd %~dp0
+
+for /f "delims=" %%x in (version) do set VERSION=%%x
+
+docker build --progress plain -t dcjulian29/elasticsearch-curator:%VERSION% .
+
+if %errorlevel% neq 0 popd;exit /b %errorlevel%
+
+popd
+
+docker tag dcjulian29/elasticsearch-curator:%VERSION%  dcjulian29/elasticsearch-curator:latest
